1d3b2e8b20ab762190485c1640b0082d4a21e558
Your Name <your-email@example.com>
2025-08-15 20:36:20 -0600

    Sprint 7: add helpers.py, data.py, main.py, .gitignore; test stubs & setup_class

---
+++ data.py
@@ -0,0 +1,12 @@
+# Test data constants for Urban Routes
+
+URBAN_ROUTES_URL = 'https://cnt-e0536884-f253-48a3-bf7a-9cce2556d13c.containerhub.tripletens-services.com/'
+
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
